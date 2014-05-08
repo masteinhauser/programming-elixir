16276eafe788343c097b5a4a4fda297b7c64e79a
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-05-08 16:08:06 +0200

    Handle cast to :increment_number

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -13,4 +13,8 @@ defmodule Sequence.Server do
   def handle_call({:set_number, new_number}, _from, _current_number) do
     { :reply, new_number, new_number }
   end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    { :noreply, current_number + delta}
+  end
 end
