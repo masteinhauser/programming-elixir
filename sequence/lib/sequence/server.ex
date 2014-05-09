b63a8817eff85bf4fd3fa503649bf9ce6492cd5b
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-05-09 15:57:36 +0200

    Create External API to wrap internal impl

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -1,6 +1,20 @@
 defmodule Sequence.Server do
   use GenServer.Behaviour
 
+  # External API
+  def start_link(current_number) do
+    :gen_server.start_link({ :local, :sequence }, __MODULE__, current_number, [])
+  end
+
+  def next_number do
+    :gen_server.call :sequence, :next_number
+  end
+
+  def increment_number(delta) do
+    :gen_server.cast :sequence, {:increment_number, delta}
+  end
+
+  # GenServer implementation
   def init(current_number)
   when is_number(current_number) do
      {:ok, current_number}
